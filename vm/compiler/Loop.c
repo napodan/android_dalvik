aosp-mirror/platform_dalvik

Say kNumDalvikInstructions when appropriate.
In particular, use it instead of just saying 256, and similarly for
255. The number of opcodes will be changing soon.

Change-Id: Icc77120c2673968dddd6b4003f717245d46e4159

vm/compiler/Loop.c
@@ -309,7 +309,7 @@ static bool doLoopBodyCodeMotion(CompilationUnit *cUnit)
             dvmCompilerDataFlowAttributes[mir->dalvikInsn.opCode];
 
         /* Skip extended MIR instructions */
-        if (dInsn->opCode > 255) continue;
+        if (dInsn->opCode >= kNumDalvikInstructions) continue;
 
         int instrFlags = dexGetInstrFlags(dInsn->opCode);
 

napodan/android_dalvik

Conflicts:
	vm/compiler/CompilerIR.h
	vm/compiler/Dataflow.c
	vm/compiler/Loop.c
	vm/compiler/codegen/arm/CodegenCommon.c
	vm/compiler/codegen/arm/CodegenDriver.c
	vm/compiler/codegen/x86/CodegenDriver.c
	vm/mterp/Mterp.c

vm/compiler/Loop.c
@@ -309,7 +309,7 @@ static bool doLoopBodyCodeMotion(CompilationUnit *cUnit)
             dvmCompilerDataFlowAttributes[mir->dalvikInsn.opcode];
 
         /* Skip extended MIR instructions */
-        if (dInsn->opcode > 255) continue;
+        if (dInsn->opcode >= kNumPackedOpcodes) continue;
 
         int instrFlags = dexGetFlagsFromOpcode(dInsn->opcode);
 